38c022828399388c1e0e79960aeba13ed51afba9
Alexander Khazan <khazanalexander@gmail.com>
2025-09-15 18:11:01 +0300

    add async logic

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using teddy_smith_api.Data;
 using teddy_smith_api.Mappers;
@@ -20,18 +21,18 @@ namespace teddy_smith_api.Controllers
     }
 
     [HttpGet]
-    public IActionResult GetAll()
+    public async Task<IActionResult> GetAll()
     {
-      var stocks = _context.Stocks.ToList()
-        .Select(s => s.ToStockDto());
+      var stocks = await _context.Stocks.ToListAsync();
+      var stockDto = stocks.Select(s => s.ToStockDto());
 
-      return Ok(stocks);
+      return Ok(stockDto);
     }
 
     [HttpGet("{id}")]
-    public IActionResult GetById([FromRoute] int id)
+    public async Task<IActionResult> GetById([FromRoute] int id)
     {
-      var stock = _context.Stocks.Find(id);
+      var stock = await _context.Stocks.FindAsync(id);
 
       if (stock == null)
         return NotFound();
@@ -40,19 +41,19 @@ namespace teddy_smith_api.Controllers
     }
 
     [HttpPost]
-    public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+    public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
     {
       var stockModel = stockDto.ToStockFromCreateDto();
-      _context.Stocks.Add(stockModel);
-      _context.SaveChanges();
+      await _context.Stocks.AddAsync(stockModel);
+      await _context.SaveChangesAsync();
       return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
     }
 
     [HttpPut]
     [Route("{id}")]
-    public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto stockDto)
+    public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto stockDto)
     {
-      var stock = _context.Stocks.Find(id);
+      var stock = await _context.Stocks.FindAsync(id);
 
       if (stock == null)
         return NotFound();
@@ -64,22 +65,22 @@ namespace teddy_smith_api.Controllers
       stock.Industry = stockDto.Industry;
       stock.MarketCap = stockDto.MarketCap;
 
-      _context.SaveChanges();
+      await _context.SaveChangesAsync();
 
       return Ok(stock.ToStockDto());
     }
 
     [HttpDelete]
     [Route("{id}")]
-    public IActionResult Delete([FromRoute] int id)
+    public async Task<IActionResult> Delete([FromRoute] int id)
     {
-      var stock = _context.Stocks.Find(id);
+      var stock = await _context.Stocks.FindAsync(id);
 
       if (stock == null)
         return NotFound();
 
       _context.Stocks.Remove(stock);
-      _context.SaveChanges();
+      await _context.SaveChangesAsync();
 
       return NoContent();
     }
