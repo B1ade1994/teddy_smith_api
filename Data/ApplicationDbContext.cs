b01a0ffc0bb45dad8ddab2ac7ce823cbf2af10ea
Alexander Khazan <khazanalexander@gmail.com>
2025-09-18 12:09:29 +0300

    add identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using teddy_smith_api.Models;
 
 namespace teddy_smith_api.Data
 {
-  public class ApplicationDbContext : DbContext
+  public class ApplicationDbContext : IdentityDbContext<User>
   {
     public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
     {
--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using teddy_smith_api.Interfaces;
 using teddy_smith_api.Repository;
 using Newtonsoft.Json;
+using teddy_smith_api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +17,36 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
   options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+  options.Password.RequireDigit = true;
+  options.Password.RequireLowercase = true;
+  options.Password.RequireUppercase = true;
+  options.Password.RequireNonAlphanumeric = true;
+  options.Password.RequiredLength = 12;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication(options => {
+  options.DefaultAuthenticateScheme =
+  options.DefaultChallengeScheme =
+  options.DefaultForbidScheme =
+  options.DefaultScheme =
+  options.DefaultSignInScheme =
+  options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => {
+  options.TokenValidationParameters = new TokenValidationParameters
+  {
+    ValidateIssuer = true,
+    ValidIssuer = builder.Configuration["JWT:Issuer"],
+    ValidateAudience = true,
+    ValidAudience = builder.Configuration["JWT:Audience"],
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = new SymmetricSecurityKey(
+      System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+    )
+  };
 });
 
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
@@ -39,6 +73,8 @@ else
   app.UseHttpsRedirection();
 }
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
