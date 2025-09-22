6f67ae115997846c9a1a6829d310c04f3686be1c
Alexander Khazan <khazanalexander@gmail.com>
2025-09-22 19:46:53 +0300

    add login api

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using teddy_smith_api.Dtos.Account;
 using teddy_smith_api.Interfaces;
 using teddy_smith_api.Models;
@@ -16,11 +17,13 @@ namespace teddy_smith_api.Controllers
   {
     public readonly UserManager<User> _userManager;
     public readonly ITokenService _tokenService;
+    public readonly SignInManager<User> _signInManager;
 
-    public AccountController(UserManager<User> userManager, ITokenService tokenService)
+    public AccountController(UserManager<User> userManager, ITokenService tokenService, SignInManager<User> signInManager)
     {
       _userManager = userManager;
       _tokenService = tokenService;
+      _signInManager = signInManager;
     }
 
     [HttpPost("register")]
@@ -68,6 +71,34 @@ namespace teddy_smith_api.Controllers
       {
         return StatusCode(500, e);
       }
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+    {
+      if (!ModelState.IsValid)
+        return BadRequest(ModelState);
+
+      var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+      if (user == null)
+        return Unauthorized("Invalid username");
+
+      var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+      if (!result.Succeeded)
+        return Unauthorized("Invalid password");
+
+      return Ok(
+        new NewUserDto
+        {
+          UserName = user.UserName,
+          Email = user.Email,
+          Token = _tokenService.CreateToken(user)
+        }
+      );
+      // if (user == null)
+      //   return Unauthorized();
     }
   }
 }
